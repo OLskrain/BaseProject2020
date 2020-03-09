2136d9636237dc7135c18cddf34e4e479f293f66
OLskrain <Olskrain2@yandex.ru>
2020-03-10 00:53:34 +0300

    BP-4 Добавил ui component, обновление даггера. Сборка падает

---
+++ app/src/main/java/com/olskrain/baseproject2020/component/ui/extension/FragmentExtensions.kt
@@ -0,0 +1,10 @@
+package com.olskrain.baseproject2020.component.ui.extension
+
+import androidx.fragment.app.Fragment
+
+/**
+ * Created by Andrey Ievlev on 10,Март,2020
+ */
+
+val Fragment.args: Args
+    get() = requireArguments()
---
+++ app/src/main/java/com/olskrain/baseproject2020/component/ui/extension/TypeAliases.kt
@@ -0,0 +1,12 @@
+package com.olskrain.baseproject2020.component.ui.extension
+
+import android.os.Bundle
+import dagger.android.support.DaggerAppCompatActivity
+
+/**
+ * Created by Andrey Ievlev on 10,Март,2020
+ */
+
+typealias Args = Bundle
+
+typealias BaseActivity = DaggerAppCompatActivity
